ee93e828399cd8b77b3d7f38632a931d89b314b1
lalawow <lalawow@gmail.com>
2017-09-20 23:09:39 +0800

    delete main.js

--- test/main.js
+++
@@ -1,7 +0,0 @@
-const stringify = require('../lib/stringify')
-
-const a = require('./source/a.json')
-console.log(stringify(a))
-
-const indent = require('./source/indent.json')
-console.log(stringify(indent, 4))
