9ccefb62ba40585426ca05984d517c5d0f2eba17
yangxiaolong <yangxiaolong@fanghuwang.com>
2017-09-27 19:04:45 +0800

    Fix

--- test/test.js
+++ test/test.js
@@ -2,10 +2,11 @@ const assert = require('assert')
 const path = require('path')
 const fs = require('fs')
 const stringify = require('../lib/stringify')
+
 describe('css stringify', function () {
   it('可以正确的 stringify 文件', function () {
     const a = require('./source/a.json')
-    assert(
+    assert.equal(
       stringify(a),
       fs.readFileSync(path.join(__dirname, './expect/a.css'), 'utf-8')
     )
@@ -13,7 +14,7 @@ describe('css stringify', function () {
 
   it('可以使用 indent', function () {
     const indent = require('./source/indent.json')
-    assert(
+    assert.equal(
       stringify(indent, 4),
       fs.readFileSync(path.join(__dirname, './expect/indent.css'), 'utf-8')
     )
